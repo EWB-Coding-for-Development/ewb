4b6dd426e9da693e419329c08b337f776a44485a
James Utter <james.utter@gmail.com>
2014-06-25 12:52:28 +1000

    Fix for bug where radio cannot be reinitialised
    * Calls to get_radio(...) with different arguments will
    return a new radio.
    * Added doctest for get_radio
    * Added __repr__ for Radio
    * Test that radio is running for radio.say() and radio.play()
    * Added RadioNotRunningError

--- src/ewb/radio.py
+++ src/ewb/radio.py
@@ -16,38 +16,49 @@ FREQUENCY_MIN, FREQUENCY_MAX = (76.0, 108.0)
 class SubprocessError(BaseException):
     pass
 
+class RadioNotRunningError(SubprocessError):
+   pass
+
 class Radio(threading.Thread):
-    def __init__(self, frequency, stereo=None, sample_rate=None):
+    def __init__(self, frequency, stereo=None, sample_rate=22050):
         threading.Thread.__init__(self)
         self.daemon = True
         assert(FREQUENCY_MIN < frequency < FREQUENCY_MAX)
         self.frequency = frequency
+        if stereo == None:
+            stereo = False
         self.stereo = stereo
-        self.sample_rate = sample_rate
+        if sample_rate == None:
+            sample_rate = 22050
+        self.sample_rate = int(sample_rate)
         self.wav_pipe_r, self.wav_pipe_w = os.pipe()
 
     def run(self):
         try:
             args = ["pifm", "-", str(self.frequency)]
             if self.sample_rate:
-                args.append(self.sample_rate)
+                args.append(str(self.sample_rate))
             if self.stereo:
-                args.append(self.stereo)
+                args.append("stereo")
 
             self.fm_process = subprocess.Popen(args, stdin=self.wav_pipe_r)
             retcode = self.fm_process.wait()
             if retcode == 255:
                 # pifm failed to open /dev/mem
-                raise SubprocessError, "User does not have write access to /dev/mem"
+                raise SubprocessError("User does not have write access to /dev/mem")
 
         except OSError as e:
             # This improves the detail given by the error on Python2.x
             if e.errno == 2:
-                print("File Not Found: '{}'".format(binary))
-                print("Make sure {} is installed and included in your PATH\n".format(binary))
                 if not e.filename:
-                    e.filename = binary # give a more descriptive file-not-found message
+                    e.filename = args[0] # give a more descriptive file-not-found message
                 raise(e)
+
+    def __repr__(self):
+        if self.is_alive():
+            return "<Radio: {}MHz {}KHz {}>".format(self.frequency, self.sample_rate, "stereo" if self.stereo else "mono")
+        else:
+            return "<Radio: Off>"
 
     def get_input_pipe(self):
         return self.wav_pipe_w
@@ -56,24 +67,30 @@ class Radio(threading.Thread):
         pitch=None, speed=None, gap=None, amplitude=None, extra_args=None,
         stdout=None, wav_fp=None, add_silence=True):
 
-        say(say_text, language=language, gender=gender, variant=variant, 
-            pitch=pitch, speed=speed, gap=gap, amplitude=amplitude, extra_args=extra_args,
-                capital_emphasis=capital_emphasis, stdout=self.wav_pipe_w)
-
-        # Play a sample of silence, to prevent pifm from looping buffer
-        if add_silence:
-            self.play_silence()
+        if self.is_alive():
+            say(say_text, language=language, gender=gender, variant=variant, 
+                pitch=pitch, speed=speed, gap=gap, amplitude=amplitude, extra_args=extra_args,
+                    capital_emphasis=capital_emphasis, stdout=self.wav_pipe_w)
+    
+            # Play a sample of silence, to prevent pifm from looping buffer
+            if add_silence:
+                self.play_silence()
+        else:
+            raise RadioNotRunningError
 
     def play(self, wav, add_silence=True):
-        if hasattr(wav, 'read'):
-            raise NotImplemented
+        if self.is_alive():
+            if hasattr(wav, 'read'):
+                raise NotImplemented
+            else:
+                player = subprocess.Popen(['cat', wav], stderr=subprocess.PIPE, stdout=self.wav_pipe_w)
+                out, err = player.communicate()
+                if player.poll():
+                    raise SubprocessError(err)
+            if add_silence:
+                self.play_silence()
         else:
-            player = subprocess.Popen(['cat', wav], stderr=subprocess.PIPE, stdout=self.wav_pipe_w)
-            out, err = player.communicate()
-            if player.poll():
-                raise SubprocessError, err
-        if add_silence:
-            self.play_silence()
+            raise RadioNotRunningError
 
     def play_silence(self):
         silence = os.path.join(os.path.dirname(__file__), "silence.wav")
@@ -83,6 +100,7 @@ class Radio(threading.Thread):
     def terminate(self):
         try:
             self.fm_process.terminate()
+            global _radio
             _radio = None
         except OSError:
             pass
@@ -90,17 +108,73 @@ class Radio(threading.Thread):
 global _radio
 _radio = None
 
-def get_radio(frequency, stereo=None, sample_rate=None):
+def get_radio(frequency, stereo=None, sample_rate=None, force=False):
+    """Starts a Raspberry Pi `pifm` radio tuned to `frequency` MHz
+
+    Get a radio tuned to 87.9 MHz
+    >>> radio = get_radio(87.9)
+    >>> radio
+    <Radio: 87.9MHz 22050KHz mono>
+
+    Broadcast "Hello World" using text-to-speech
+    >>> radio.say("Hello World!")
+
+    Play a WAV audio file
+    >>> radio.play("/usr/share/pyshared/pygame/examples/data/secosmic_lo.wav")
+
+    Get a stereo radio, tuned to 107.5 MHz at a sample rate of 44100
+    >>> radio = get_radio(107.5, True, 44100)
+    >>> radio
+    <Radio: 107.5MHz 44100KHz stereo>
+
+    >>> radio.frequency
+    107.5
+    >>> radio.stereo
+    True
+    >>> radio.sample_rate
+    44100
+
+    Returns <Radio>"""
+
     global _radio
+
+    try:
+        # if there is already a Radio, but the settings have changed, kill it.
+        if _radio.frequency != frequency:
+            _radio.terminate()
+        if _radio.stereo != stereo:
+            _radio.terminate()
+        if _radio.sample_rate != sample_rate:
+            _radio.terminate()
+
+        # if `force` is set, kill the existing radio
+        if force:
+            _radio.terminate()
+    except AttributeError:
+        pass
+
+    # Start a Radio if we don't have one
     if _radio == None:
-        radio = Radio(frequency, stereo, sample_rate)
-        radio.start()
-        _radio = radio
-    return _radio
+        _radio = Radio(frequency, stereo, sample_rate)
+        _radio.start()
+
+    if _radio.is_alive():
+        return _radio
+    else:
+        print("Radio initialisation failed")
+        return None
 
 def kill_children():
     """Handler to kill child processes at exit"""
     time.sleep(1) # wait a second so the audio transmission completes
-    _radio.terminate()
+    try:
+        _radio.terminate()
+    except AttributeError:
+        # radio not initialised, nothing to terminate
+        pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 atexit.register(kill_children)
